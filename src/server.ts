f194035e21d0018a9ab2cf239317425224d96464
Jeroen Claassens <support@favware.tech>
2021-10-17 14:51:54 +0200

    fix: fixed server init [skip publish]

--- src/server.ts
+++ src/server.ts
@@ -43,6 +43,7 @@ const gqlServer = async (): Promise<Koa<Koa.DefaultState, Koa.DefaultContext>> =
 		]
 	});
 
+	await apolloServer.start();
 	apolloServer.applyMiddleware({ app, path: '/', cors: true });
 
 	return app;
